4c505c90c3cce103c4e87636e359bbabd10466c8
Stratigos <Stratigos@users.noreply.github.com>
2015-05-09 17:37:12 -0400

    updating integration test value

--- test/integration/list_all_states_test.rb
+++ test/integration/list_all_states_test.rb
@@ -4,7 +4,6 @@ class ListAllStatesTest < ActionDispatch::IntegrationTest
   # Makes a request to the main API endpoint, checks for expected response
   #  code, JSON content-type, correct number of States, and that the States'
   #  data is not empty.
-  # Two States are created in ../fixtures/states.yml.
   test 'Lists All States and Gas Prices' do
     get '/api/v1/states'
 
@@ -12,7 +11,7 @@ class ListAllStatesTest < ActionDispatch::IntegrationTest
 
     assert_equal     200,        response.status
     assert_equal     Mime::JSON, response.content_type
-    assert_equal     2,          states.length
+    assert_equal     50,          states.length
     assert_not_empty states[0][:name]
     assert_not_empty states[0][:price]
     assert_not_empty states[1][:name]
